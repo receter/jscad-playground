8ac4166b3ade1f530c4df6e36ee88f5987946486
Andreas Riedmüller <receter@gmail.com>
2025-02-16 16:35:43 +0100

    feat: set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // set base for github pages
+  base: "/jscad-playground/",
   plugins: [react()],
 });
